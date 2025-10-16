2f83dbd825a60fabd7d04bdf5be02c10aa05a9de
Hooper <brenton@ccpgames.com>
2025-10-16 15:08:18 +1000

    Adding argparse and config opions, along with a couple of new stories

--- StoryNetwork/story_network.py
+++ StoryNetwork/story_network.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import logging
 
+NODE_SCALE_FACTOR = 10
 
 class StoryNetwork:
     def __init__(self):
@@ -9,8 +10,10 @@ class StoryNetwork:
     def add_node(self, node_id):
         self.G.add_node(node_id)
 
+
     def add_edge(self, source_node_id, target_node_id):
         self.G.add_edge(source_node_id, target_node_id)
+
 
     def remove_unconnected_nodes(self):
         pre_count = self.G.number_of_nodes()
@@ -18,8 +21,47 @@ class StoryNetwork:
         post_count = self.G.number_of_nodes()
         logging.info(f"Removed {pre_count - post_count} unconnected nodes.")
 
+
     def get_graph(self):
         return self.G
 
+
     def add_node_attributes(self, all_attributes):
         nx.set_node_attributes(self.G, all_attributes)
+
+
+    def _add_all_nodes(self, story_catalog):
+        for story_id in story_catalog.get_all_story_ids():
+            self.add_node(story_id)
+        logging.info(f"Added {self.G.number_of_nodes()} nodes to the story graph.")
+
+
+    def _add_all_edges(self, story_catalog):
+        for source_id, target_id in story_catalog.get_all_story_connections():
+            self.add_edge(source_id, target_id)
+        logging.info(f"Added {self.G.number_of_edges()} edges to the story graph.")
+
+
+    def _add_node_attributes(self, story_catalog):
+        for node_id in self.G.nodes():
+            node = self.G.nodes[node_id]
+            story = story_catalog.get_story(node_id)
+            if story:
+                node['name'] = story.get('name', 'Unknown')
+                node['author'] = story.get('author', 'Unknown')
+                node['link'] = story.get('link', 'Unknown')
+                edge_count = len(list(self.G.in_edges(node_id))) + len(list(self.G.out_edges(node_id)))
+
+                node['node_click'] = f"test words"
+                node['size'] = edge_count * NODE_SCALE_FACTOR
+
+                logging.debug(f"Node {node_id}: {node['name']} by {node['author']} ({node.get('year', 'Unknown')}) with {edge_count} connections")
+            else:
+                logging.warning(f"No story found for node ID: {node_id}")
+
+
+    def add_all_stories(self, story_catalog):
+        self._add_all_nodes(story_catalog)
+        self._add_all_edges(story_catalog)
+        self._add_node_attributes(story_catalog)
+        # self.remove_unconnected_nodes()
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import logging
-import yaml
-import os
-
-import networkx as nx
-import matplotlib.pyplot as plt
+import argparse
 
 from StoryCatalog import StoryCatalog
 from StoryNetwork import StoryNetwork
 
-import plotly.graph_objects as go
 import gravis as gv
 
 
@@ -22,46 +17,19 @@ logging.basicConfig(
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", default='graph.html', type=str, help="Output graph HTML file name")
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.INFO)
 
     story_catalog = StoryCatalog()
     story_network = StoryNetwork()
 
     story_catalog.load_all_stories()
+    story_network.add_all_stories(story_catalog)
 
-    for story_id in story_catalog.get_all_story_ids():
-        story_network.add_node(story_id)
-
-    for source_id, target_id in story_catalog.get_all_story_connections():
-        story_network.add_edge(source_id, target_id)
-
-    # story_network.add_node_attributes(story_catalog.stories)
-
-
-    story_network.remove_unconnected_nodes()
     G = story_network.get_graph()
-    print("NODE COUNT: ", G.number_of_nodes())
-
-    for node_id in G.nodes():
-        node = G.nodes[node_id]
-        node['name'] = story_catalog.get_story(node_id)['name']
-        node['author'] = story_catalog.get_story(node_id).get('author', 'Unknown')
-        node['year'] = story_catalog.get_story(node_id).get('year', 'Unknown')
-
-        edge_count = len(list(G.in_edges(node_id))) + len(list(G.out_edges(node_id)))
-        node['size'] = edge_count * NODE_SCALE_FACTOR
-
-    
-    # Directed Graph
-    # plt.figure(1, figsize=(16, 16))
-    # nx.draw(G, pos=nx.spring_layout(G, gravity=0.1), labels=story_catalog.get_story_name_mapping(G.nodes()), with_labels=True, font_size=12)
-    # plt.show()
-
-
-    # Shell Layout
-    # plt.figure(2, figsize=(16, 16))    # plt.figure(3, figsize=(16, 16))
-    # nx.draw(G, pos=nx.shell_layout(G), labels=story_catalog.get_story_name_mapping(G.nodes()), with_labels=True, font_size=12)
-    # plt.show()
 
     # Gravis
     fig = gv.d3(
@@ -72,13 +40,17 @@ if __name__ == "__main__":
 
         graph_height=1200,
 
+        node_size_factor=1,
         use_node_size_normalization=True,
-        node_size_normalization_max=30,
-        edge_size_data_source='weight',
+        node_size_normalization_min=5,
+        node_size_normalization_max=100,
+
+
+        # edge_size_data_source='weight',
         edge_curvature=0.3,
 
         use_collision_force=True,
         collision_force_radius=50,
         collision_force_strength=0.7,
     )
-    fig.export_html('graph.html', overwrite=True)
+    fig.export_html(args.file, overwrite=True)
